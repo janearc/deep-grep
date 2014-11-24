4fe24c7541049df18c5583e3adcb499eb88729f4
jane avriette <jane@cpan.org>
2014-11-24 10:17:22 -0500

    add test for nested lists re-structure examine() to return true in the right scope added note about the test for chai.assert.include not being quite what it says it is

--- test/grep.js
+++ test/grep.js
@@ -16,10 +16,39 @@ it( 'syntax', function () {
 } );
 
 it( 'simple list', function () {
+	dg = require( '../lib/dg.js' );
+
 	assert( function () {
 		var uni = dg.deeply( simple_list, function (t) { if (t == 'unicorn') return true } );
 		if (uni.length != 1) return false;
 		if (uni[0] != 'unicorn') return false;
 		return true;
-	}, 'returns requisite element' )
+	}, 'returns requisite element' );
+
+} );
+
+it( 'nested list', function () {
+	dg = require( '../lib/dg.js' );
+
+	assert( function () {
+		var uni = dg.deeply( nested_list, function (t) { if (t == 'unicorn') return true } );
+		if (uni.length != 1) return false;
+		if (uni[0] != 'unicorn') return false;
+		return true;
+	}, 'returns requisite element' );
+
+	var uni = dg.deeply( nested_list, function (t) {
+		if ( (t == 'unicorn') || (t == 'leprechaun') ) { return true }
+	} );
+
+	console.log( uni );
+	assert(uni.length == 2, 'two elements returned');
+
+	// XXX: This is kind of cheating. It looks like 'include' does not mean 'equal':
+	//   AssertionError: correct list values returned: expected [ 'unicorn', 'leprechaun' ] to include [ 'unicorn', 'leprechaun' ]
+	// so for now we just get a haystack and check for needles. Eventually this
+	// needs to be fixed.
+	//
+
+	chai.assert.include( mythical, uni, 'correct list values returned' );
 } );
