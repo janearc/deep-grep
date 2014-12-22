6389f39bd9ce1489514acf907532c001c8880caa
jane avriette <jane@cpan.org>
2014-12-22 14:19:48 +0700

    this fixes the outstanding issues with tests this fixes the 'how do i tell if that's an array' bit i'm going to commit, push, and merge this, so i can get started on sendak. again.

--- test/synchronous.js
+++ test/synchronous.js
@@ -5,7 +5,7 @@ var input    = [ 'test', 'another test', 1234, 'a string' ]
 	, results = [ ];
 
 it('Expression (synchronous): object returned', function () {
-	var r = dg.sync( input, new RegExp( '^test$' ) );
+	var r = dg.sync( input, /^test$/ );
 	assert.equal( typeof r, 'object' )
 } );
 
@@ -14,10 +14,10 @@ it('Expression (synchronous): correct value returned', function () {
 } );
 
 it('Function (synchronous): object returned', function () {
-	var r = dg.sync( input, function (t) { if ( t == 'test' ) return 1 } );
-	assert.equal( typeof r, 'object' )
+	var r = dg.sync( input, function (t) { if ( t == 'test' ) { return 1 } } );
+	assert( (typeof r) ==  'object', 'object returned' )
 } );
 
 it('Function (synchronous): correct value returned', function () {
-	assert.deepEqual( dg.sync( input, function (t) { if (t == 'test') return 1 } ) , [ 'test' ] )
+	assert.deepEqual( dg.sync( input, function (t) { if (t === 'test') { return 1 } } ) , [ 'test' ] )
 } );
