f3dbb95a74027d89b379e866933b21fad1ab6fbe
jane arc <jane@cpan.org>
2015-01-14 10:03:13 -0500

    re-broke unit tests because deeply() now does the right thing

--- test/hash.js
+++ test/hash.js
@@ -25,8 +25,7 @@ it('find a username in a hash', function () {
 			'check-values': true
 		}
 	);
-	assert(1)
-	//assert.equal( r, { 'user-name': 'tybalt' } )
+	assert.equal( r, { 'user-name': 'tybalt' } )
 } );
 
 // jane@cpan.org // vim:tw=80:ts=2:noet
